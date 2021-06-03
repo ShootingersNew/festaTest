c1eee56e14f2a0c4d789c84b60e0f7ad778405ae
ShootingersNew <lebedevseergey@gmail.com>
2021-06-03 12:40:43 +0300

    add drop container styles

--- src/components/DropContainer/DropContainer.vue
+++ src/components/DropContainer/DropContainer.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="dropContainer">container</div>
+  <div class="dropContainer">
+    <div class="dropContainer__title">Container A</div>
+    <div class="dropContainer__container">container</div>
+  </div>
 </template>
-
+<style>
+.dropContainer__container {
+  width: 100%;
+  height: 500px;
+  position: relative;
+  background-color: #eeeeee;
+  border: 2px solid #888888;
+}
+.dropContainer__title {
+  font-size: 1.5em;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 1.75rem;
+  margin: 0.67em 0;
+  color: #888888;
+}
+</style>
 <script lang="ts">
 import { Component } from "vue-property-decorator";
 import Vue from "vue";
